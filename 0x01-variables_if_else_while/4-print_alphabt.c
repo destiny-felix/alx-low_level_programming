a84f8038e33ba832049f95908e46f3d8b030b40c
Kiisi <destinyfelixk06@gmail.com>
2022-07-04 06:12:20 -0700

    DOne

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'e' || a == 'q')
+			continue;
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}

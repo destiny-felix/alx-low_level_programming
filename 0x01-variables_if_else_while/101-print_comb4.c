1e4c5e29eb26f2f33ecf6a80f9c1f80b1580c7ef
Kiisi <destinyfelixk06@gmail.com>
2022-07-04 08:02:52 -0700

    Done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if ((i == j) || (j == k) || (i == k) || (j > k) || (i > j))
+					continue;
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				
+				if (i == 55 && j == 56 && k == 57)
+					continue;
+				else{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

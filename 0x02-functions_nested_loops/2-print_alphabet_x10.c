55bbac5a72d3b31ddc84022589753f7ec5359c91
Kiisi <destinyfelixk06@gmail.com>
2022-07-06 23:46:51 -0700

    DOne

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10X
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		j = 97;
+
+		while (j <= 122)
+		{
+			_putchar(j);
+			j++;
+		}
+		i++;
+	}
+}

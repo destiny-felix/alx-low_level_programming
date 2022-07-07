200038b0765ccf7570eb78a31e32ab0b046ca07e
Kiisi <destinyfelixk06@gmail.com>
2022-07-06 23:24:59 -0700

    Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n')
+}

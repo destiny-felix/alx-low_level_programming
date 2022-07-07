18647be7473d5a2e3516c15c425758b5ad8bc77a
Kiisi <destinyfelixk06@gmail.com>
2022-07-06 23:15:22 -0700

    Done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * _putchar: output char
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+
+	int i;
+	
+	i = 0;
+
+	while (i < 8)
+	{
+		_putchar(text[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}

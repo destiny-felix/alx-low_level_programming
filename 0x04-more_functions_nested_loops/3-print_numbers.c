739a2ca00113f84369ddbb1a05b8ca393cb5a23d
Kiisi <destinyfelixk06@gmail.com>
2022-07-08 04:27:44 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print_numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar(10);
+}

2466ffc5ab9974359451684d6d830728b17c5b19
Kiisi <destinyfelixk06@gmail.com>
2022-07-07 19:00:55 -0700

    DOne

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,14 +17,18 @@ void print_to_98(int n)
 
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
 	else
 	{
 		for (; n >= 98; n--)
 		{
-			printf("%d, ", n);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
 		}
 	}
 }

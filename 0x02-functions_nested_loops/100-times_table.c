46dfd9c0bf794ac020d0f34ca9b2fe10adb3d386
Kiisi <destinyfelixk06@gmail.com>
2022-07-07 19:16:49 -0700

    Done

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_times_table - print_times_table
+ * @n: n
+ * Return: void
+ */
+void print_times_table(int n);
+{
+	int i, j;
+
+	i = n, j = n;
+
+	printf("%d", i);
+}

a9f79777cfb78722a00be699dd2a2b46cbc30185
Kiisi <destinyfelixk06@gmail.com>
2022-07-07 19:37:14 -0700

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,5 +7,26 @@
  */
 void print_times_table(int n)
 {
-	printf("%d", n);
+	int i, j, mul;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i  <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				mul = i * j;
+
+				printf("%d", mul);
+
+				if (mul < 10 && j < n)
+					printf(",   ");
+				else if (mul >= 10 && j < n)
+					printf(",  ");
+				else if (mul >= 100 && j < n)
+					printf(", ");
+			}
+			printf("\n");
+		}
+	}
 }

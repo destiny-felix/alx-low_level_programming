53724c748d7f73c81108f34bded07b55827e6ae4
Kiisi <destinyfelixk06@gmail.com>
2022-07-08 04:39:01 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print_most_numbers
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		_putchar(i);
+	}
+}

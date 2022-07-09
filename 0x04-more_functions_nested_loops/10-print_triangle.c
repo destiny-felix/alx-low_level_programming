cc6cd6ad2e865449fae6bb91d8173c2fb2133656
Kiisi <destinyfelixk06@gmail.com>
2022-07-09 12:46:40 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_triangle - print_triangle
+ * @size: size
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (j <= size - i)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar(10);
+		}
+	}
+}

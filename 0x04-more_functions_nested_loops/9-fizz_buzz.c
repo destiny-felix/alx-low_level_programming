b56cd3db6271d40be9abd07bafd65f4304bf775e
Kiisi <destinyfelixk06@gmail.com>
2022-07-09 11:59:35 -0700

    Done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,10 @@ int main(void)
 	{
 		if (i % 3 == 0)
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i != 100)
 			printf("Buzz ");
+		else if (i % 5 == 0 && i == 100)
+			printf("Buzz");
 		else if (i % 5 == 0 && i % 3 == 0)
 			printf("Fizz Buzz ");
 		else

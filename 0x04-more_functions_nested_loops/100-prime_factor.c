6c4173108119ef851366b62daf45feb6b71b82b0
Kiisi <destinyfelixk06@gmail.com>
2022-07-09 13:25:32 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main
+ * Return: 0
+ */
+int main(void)
+{
+	long prime_num = 612852475143;
+	long divisor = 2;
+
+	while (num != 0)
+	{
+		if (num % divisor == 0)
+		{
+			num /= divisor;
+			large_num = divisor;
+		}
+		divisor += 1;
+	}
+	return (0);
+}

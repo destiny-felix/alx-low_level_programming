6dcd092bb71419798561cc15654d3118569650cc
Kiisi <destinyfelixk06@gmail.com>
2022-07-12 10:59:40 -0700

    DOne

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - _puts
+ * @str: str
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+}

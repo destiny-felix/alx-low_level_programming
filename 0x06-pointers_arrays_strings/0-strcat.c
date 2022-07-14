a29f327b33050b513d28b8a8e0a40929b892377d
Kiisi <destinyfelixk06@gmail.com>
2022-07-14 16:51:41 -0700

    Done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcat - _strcat
+ * @dest: dest
+ * @src: src
+ * Return: char*
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0, j = 0;
+
+	while (dest[i])
+	{
+		i++
+	}
+
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
